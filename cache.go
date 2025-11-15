0805286b6536996d160b8cf6ef6af0729ff9169f
Your Name <you@example.com>
2025-11-15 17:37:25 -0500

    make the refresh time and the keep time defined at init

--- cache.go
+++ cache.go
@@ -1,6 +1,9 @@
 package cache
 
 import (
+	"context"
+	"runtime"
+	"sync"
 	"time"
 	"unsafe"
 
@@ -14,11 +17,14 @@ type (
 		constraints.Integer | constraints.Float | constraints.Complex | ~string | uintptr | ~unsafe.Pointer
 	}
 
-	// Cache struct holds the cache data structure and configuration
+	// Cache holds the cache data structure and configuration
 	Cache[K hashable, V any] struct {
-		cacheMap    *haxmap.Map[K, *element[V]] // Map to store key-value pairs
-		RefreshTime time.Duration               // How often to refresh cache entries
-		refreshFunc func(K) V                   // Function to generate new values
+		cacheMap    *haxmap.Map[K, *element[V]]                  // Map to store key-value pairs
+		RefreshTime time.Duration                                // How often to refresh cache entries
+		KeepTime    time.Duration                                // How long to keep cache entries before deleting
+		refreshFunc func(context.Context, K) (val V, store bool) // Function to generate new values
+		ctx         context.Context                              // Flag to indicate if cache is active
+		cancel      context.CancelFunc
 	}
 
 	// element struct represents a single cache entry
@@ -28,34 +34,72 @@ type (
 		created  time.Time     // When the entry was created
 		ready    chan struct{} // Channel to signal when data is ready
 	}
+
+	// CacheMap holds the cache data structure and configuration
+	CacheMap[K hashable, V any] struct {
+		cacheMap    *haxmap.Map[K, *mapElement[V]]                 // Map to store key-value pairs
+		RefreshTime time.Duration                                  // How often to refresh cache entries
+		KeepTime    time.Duration                                  // How long to keep cache entries before deleting
+		lastRefresh time.Time                                      // Time of the last refresh
+		refreshFunc func(context.Context, func(K, V)) (store bool) // Function to generate all new values
+		ctx         context.Context                                // Flag to indicate if cache is active
+		cancel      context.CancelFunc
+
+		ready chan struct{} // Channel to signal when data is ready
+	}
+
+	// element struct represents a single cache entry
+	mapElement[V any] struct {
+		data    V         // The cached data
+		created time.Time // When the entry was created
+	}
 )
 
 // New creates a new cache instance with specified refresh time and refresh function
-func New[K hashable, V any](RefreshTime time.Duration, refreshFunc func(K) V) *Cache[K, V] {
+func New[K hashable, V any](RefreshTime, KeepTime time.Duration,
+	refreshFunc func(context.Context, K) (V, bool)) *Cache[K, V] {
+
 	// Initialize new cache with provided parameters
 	c := &Cache[K, V]{
 		cacheMap:    haxmap.New[K, *element[V]](),
 		RefreshTime: RefreshTime,
+		KeepTime:    KeepTime,
 		refreshFunc: refreshFunc,
 	}
+	c.ctx, c.cancel = context.WithCancel(context.Background())
+
+	runtime.AddCleanup(c, func(int) {
+		c.cancel()
+		c.cacheMap.Clear()
+	}, 0)
 
 	// Start background goroutine for cache maintenance
 	go func() {
-		for {
-			// Sleep for 1/8th of refresh time between maintenance cycles
-			time.Sleep(c.RefreshTime >> 3)
+		for c.ctx.Err() == nil {
+			// Sleep for 1/4th of refresh time between maintenance cycles
+			time.Sleep(c.RefreshTime >> 2)
+
+			// Test if c.ctx is done
+			if c.ctx.Err() != nil {
+				break
+			}
 
 			// Track keys that need to be deleted
 			var toDelete []K
 
 			// Iterate through all cache entries
 			c.cacheMap.ForEach(func(key K, value *element[V]) bool {
+				// Test if c.ctx is done
+				if c.ctx.Err() != nil {
+					return false
+				}
+
 				sinceCreated := time.Since(value.created)
 
-				if sinceCreated > c.RefreshTime { // Remove entries older than must-refresh-time
+				if c.KeepTime > 0 && sinceCreated > c.KeepTime { // Remove entries older than must-refresh-time
 					toDelete = append(toDelete, key)
 
-				} else if sinceCreated < c.RefreshTime>>1 { // If this is a fresh entry
+				} else if sinceCreated < c.RefreshTime { // If this is a fresh entry
 					// No operation needed
 
 				} else if value.created.After(value.lastUsed) { // If entry has not been used in a while
@@ -63,8 +107,13 @@ func New[K hashable, V any](RefreshTime time.Duration, refreshFunc func(K) V) *C
 					// TODO: Consider staling out data early to save memory
 
 				} else if time.Since(value.lastUsed) < c.RefreshTime>>1 {
+					withTimeout, _ := context.WithTimeout(c.ctx, c.RefreshTime>>1)
+
 					// Start a refresh for ensuring data is still fresh and relevant
-					data := refreshFunc(key)
+					data, ok := refreshFunc(withTimeout, key)
+					if !ok {
+						return true
+					}
 					value.data, value.created = data, time.Now()
 				}
 				return true
@@ -72,33 +121,158 @@ func New[K hashable, V any](RefreshTime time.Duration, refreshFunc func(K) V) *C
 			// Delete all expired entries
 			c.cacheMap.Del(toDelete...)
 		}
+
+		c.cacheMap.Clear()
+		runtime.GC()
 	}()
 	return c
 }
 
 // Get retrieves a value from the cache by key
-func (c *Cache[K, V]) Get(key K) (data V, fnCalled bool) {
+func (c *Cache[K, V]) Get(ctx context.Context, key K) (data V, ready bool) {
 	// Try to get value from cache
 	value, loaded := c.cacheMap.GetOrCompute(key, func() *element[V] {
 		// If not found, create a new entry
 		return &element[V]{
-			lastUsed: time.Now(),
-			created:  time.Now(),
-			ready:    make(chan struct{}, 1),
+			created: time.Now(),
+			ready:   make(chan struct{}, 1),
 		}
 	})
 
 	if loaded {
 		// Wait for data to be ready
-		<-value.ready
+		// If ctx is cancelled or c is not ready
+		if value.ready != nil {
+			select {
+			case <-ctx.Done(): // return immediately
+				return value.data, false
+			case <-value.ready: // wait for the map to be populated
+			}
+		}
+
+		if value.lastUsed.IsZero() {
+			return value.data, false
+		}
 		value.lastUsed = time.Now()
-		return value.data, false
+		return value.data, true
 	}
 
 	// Signal that data is ready on close
 	defer close(value.ready)
 
 	// Pull the data and set the data
-	value.data, value.created = c.refreshFunc(key), time.Now()
-	return value.data, true
+	data, ok := c.refreshFunc(ctx, key)
+	if ok {
+		value.data, value.lastUsed = data, time.Now()
+	}
+	return value.data, ok
+}
+
+// Set manually add a value to the cache for use
+func (c *Cache[K, V]) Set(key K, value V) {
+	now := time.Now()
+	elm := &element[V]{
+		data:     value,
+		created:  now,
+		lastUsed: now,
+	}
+	c.cacheMap.Set(key, elm)
+}
+
+// New creates a new cache instance with specified refresh time and refresh function
+func NewMap[K hashable, V any](RefreshTime, KeepTime time.Duration,
+	refreshFunc func(context.Context, func(K, V)) bool) *CacheMap[K, V] {
+	// Initialize new cache with provided parameters
+	c := &CacheMap[K, V]{
+		cacheMap:    haxmap.New[K, *mapElement[V]](),
+		RefreshTime: RefreshTime,
+		KeepTime:    KeepTime,
+		refreshFunc: refreshFunc,
+		ready:       make(chan struct{}),
+	}
+	c.ctx, c.cancel = context.WithCancel(context.Background())
+	ready := sync.OnceFunc(func() {
+		close(c.ready)
+	})
+
+	runtime.AddCleanup(c, func(int) {
+		c.cancel()
+		c.cacheMap.Clear()
+	}, 0)
+
+	// Start background goroutine for cache maintenance
+	go func() {
+		defer ready() // If the service is cancelled, release any holds
+
+		start := time.Now() // Mark the start of the refresh interval
+		if c.refreshFunc(c.ctx, func(key K, val V) {
+			c.cacheMap.Set(key, &mapElement[V]{
+				data:    val,
+				created: time.Now(),
+			})
+		}) && c.ctx.Err() == nil {
+			c.lastRefresh = start
+			ready()
+		}
+
+		for c.ctx.Err() == nil {
+			// Sleep for 1/4th of refresh time between maintenance cycles
+			time.Sleep(c.RefreshTime >> 2)
+
+			if c.ctx.Err() != nil {
+				return
+			}
+
+			// Sleep for 1/4th of refresh time between maintenance cycles
+			time.Sleep(c.RefreshTime >> 4)
+
+			// Track keys that need to be deleted
+			var toDelete []K
+
+			// Iterate through all cache entries
+			c.cacheMap.ForEach(func(key K, value *mapElement[V]) bool {
+				sinceCreated := time.Since(value.created)
+
+				if sinceCreated > c.KeepTime { // Remove entries older than must-refresh-time
+					toDelete = append(toDelete, key)
+				}
+				return true
+			})
+			// Delete all expired entries
+			c.cacheMap.Del(toDelete...)
+
+			if time.Since(c.lastRefresh) < c.RefreshTime {
+				continue
+			}
+
+			start := time.Now() // Mark the start of the refresh interval
+			if c.refreshFunc(c.ctx, func(key K, val V) {
+				c.cacheMap.Set(key, &mapElement[V]{
+					data:    val,
+					created: time.Now(),
+				})
+			}) && c.ctx.Err() == nil {
+				c.lastRefresh = start
+				ready()
+			}
+		}
+	}()
+	return c
+}
+
+// Get retrieves a value from the cache by key
+func (c *CacheMap[K, V]) Get(ctx context.Context, key K) (data V, found bool) {
+	// If ctx is cancelled or c is not ready
+	select {
+	case <-ctx.Done(): // return immediately
+		return
+	case <-c.ready: // wait for the map to be populated
+	}
+
+	// Try to get value from cache
+	value, loaded := c.cacheMap.Get(key)
+	if loaded {
+		return value.data, true
+	}
+	return
 }
--- cache_test.go
+++ cache_test.go
@@ -1,6 +1,8 @@
 package cache_test
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"testing"
 	"time"
@@ -8,18 +10,55 @@ import (
 	cache "github.com/pschou/go-cachefn"
 )
 
-func TestCacheMap(t *testing.T) {
+func TestCache(t *testing.T) {
 	// Create a map to serve as our cache
-	cache := cache.New[string, int](3*time.Second, func(s string) int {
-		//log.Println("  refreshing", s)
-		return len(s)
+	cache := cache.New[string, int](3*time.Second, time.Hour, func(ctx context.Context, s string) (int, bool) {
+		log.Println("  refreshing", s)
+		return len(s), true
 	})
 
-	one, ok := cache.Get("one")
+	ctx := context.Background()
+
+	one, ok := cache.Get(ctx, "one")
 	log.Println("one:", one, ok)
-	one, ok = cache.Get("one")
+	one, ok = cache.Get(ctx, "one")
 	log.Println("one:", one, ok)
+
+	log.Println("sleep 2")
+	time.Sleep(2 * time.Second)
+
+	one, ok = cache.Get(ctx, "one")
+	log.Println("one:", one, ok)
+
+	log.Println("sleep 5")
 	time.Sleep(5 * time.Second)
-	one, ok = cache.Get("one")
+
+	one, ok = cache.Get(ctx, "one")
 	log.Println("one:", one, ok)
+}
+
+func TestCacheMap(t *testing.T) {
+	// Create a map to serve as our cache
+	cache := cache.NewMap[string, int](4*time.Second, time.Hour, func(ctx context.Context, set func(s string, v int)) bool {
+		log.Println("  refreshing map")
+		time.Sleep(1 * time.Second)
+		for i := 0; i < 10 && ctx.Err() == nil; i++ {
+			set(fmt.Sprintf("%d", i), i)
+		}
+		return true
+	})
+
+	ctx := context.Background()
+
+	log.Println("looking up 1,2,3")
+	one, ok := cache.Get(ctx, "1")
+	log.Println("1:", one, ok)
+	one, ok = cache.Get(ctx, "2")
+	log.Println("2:", one, ok)
+
+	log.Println("sleep 6")
+	time.Sleep(6 * time.Second)
+
+	one, ok = cache.Get(ctx, "3")
+	log.Println("3:", one, ok)
 }
